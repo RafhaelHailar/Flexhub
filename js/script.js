f87c625cd437dc25262876f6f31b138a5597060b
Rafhael <rafhaelhailar@yahoo.com>
2023-08-17 22:14:11 +0800

    Wallop

--- js/script.js
+++ js/script.js
@@ -46,16 +46,20 @@ let current_page = 1;
         breakpoints: {
             // when window width is >= 320px
             320: {
-              slidesPerView: 2,
-              spaceBetween: 20
+              slidesPerView: 1,
+              spaceBetween: 10
             },
             // when window width is >= 480px
-            480: {
-              slidesPerView: 3,
+            800: {
+              slidesPerView: 2,
               spaceBetween: 30
             },
             // when window width is >= 640px
-            640: {
+            1000 : {
+              slidesPerView: 3,
+              spaceBetween: 40
+            },
+            1400 : {
               slidesPerView: 4,
               spaceBetween: 40
             }
